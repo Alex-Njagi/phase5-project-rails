33362a10d7a82246c7cce8e9292d7f88efc318c5
lewisky <lewisky28mawira@gmail.com>
2023-08-01 15:18:37 +0300

    added index method in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy]
 
-  resources :farmer_products, only: [:show, :create, :update, :destroy]
+  resources :farmer_products, only: [:index, :show, :create, :update, :destroy]
 
-  resources :farmer_product_sales, only: [:show, :create, :update, :destroy]
+  resources :farmer_product_sales, only: [:index, :show, :create, :update, :destroy]
 
-  resources :farmer_produce_sales, only: [:show, :create, :update, :destroy]
+  resources :farmer_produce_sales, only: [:index, :show, :create, :update, :destroy]
 
-  resources :farmer_trainings, only: [:show, :create, :update, :destroy]
+  resources :farmer_trainings, only: [:index, :show, :create, :update, :destroy]
 
-  resources :farming_lands, only: [:show, :create, :update, :destroy]
+  resources :farming_lands, only: [:index, :show, :create, :update, :destroy]
 
-  resources :land_operations, only: [:show, :create, :update, :destroy]
+  resources :land_operations, only: [:index, :show, :create, :update, :destroy]
 
 
 end
