797ae015318ecdd1a1a61029f0f62a0745323571
lewisky <lewisky28mawira@gmail.com>
2023-08-01 14:01:21 +0300

    created farmer product sales controller

---
+++ app/controllers/farmer_product_sales_controller.rb
@@ -0,0 +1,2 @@
+class FarmerProductSalesController < ApplicationController
+end
---
+++ test/controllers/farmer_product_sales_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FarmerProductSalesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
