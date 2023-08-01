66aaa430b4dac7f1945dc6ffbdb4979b5f1d987f
lewisky <lewisky28mawira@gmail.com>
2023-08-01 14:03:59 +0300

    created POST /farmer_product_sales in farmer product sales controller

--- app/controllers/farmer_product_sales_controller.rb
+++ app/controllers/farmer_product_sales_controller.rb
@@ -5,4 +5,15 @@ class FarmerProductSalesController < ApplicationController
   def show
     render json: @product_sale
   end
+
+  # POST /farmer_product_sales
+  def create
+    @product_sale = FarmerProductSale.new(product_sale_params)
+
+    if @product_sale.save
+      render json: @product_sale, status: :created
+    else
+      render json: { errors: @product_sale.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
