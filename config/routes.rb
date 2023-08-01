1adbcdd31ce0d2d2f196cf55d9820daa5b36d596
lewisky <lewisky28mawira@gmail.com>
2023-08-01 14:39:24 +0300

    completed farmer training routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
 
   resources :farmer_produce_sales, only: [:show, :create, :update, :destroy]
 
+  resources :farmer_trainings, only: [:show, :create, :update, :destroy]
+
+  
 
 end
