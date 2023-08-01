25f28a21b9d23dc42f21e09e9c83e9f6b27c1ae0
lewisky <lewisky28mawira@gmail.com>
2023-08-01 13:49:38 +0300

    completed GET /farmer_trainings/:id farmer training controller

--- app/controllers/farmer_trainings_controller.rb
+++ app/controllers/farmer_trainings_controller.rb
@@ -1,2 +1,10 @@
 class FarmerTrainingsController < ApplicationController
+    before_action :set_farmer_training, only: [:show, :update, :destroy]
+
+  # GET /farmer_trainings/:id
+  def show
+    render json: @farmer_training
+  end
+
+  
 end
