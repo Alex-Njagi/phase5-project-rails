ae34c35b03b8a0271dfcc1a341f9ca8e33891140
lewisky <lewisky28mawira@gmail.com>
2023-08-01 14:14:04 +0300

    created GET /farmer_produce_sales/:id in farmer produce sales controller

--- app/controllers/farmer_produce_sales_controller.rb
+++ app/controllers/farmer_produce_sales_controller.rb
@@ -1,2 +1,8 @@
 class FarmerProduceSalesController < ApplicationController
+    before_action :set_produce_sale, only: [:show, :update, :destroy]
+
+  # GET /farmer_produce_sales/:id
+  def show
+    render json: @produce_sale
+  end
 end
